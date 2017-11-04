633651b49e6a348667a7d7d2f8a4d2a56442914f
Myriam Benkoussa <benkoussam@sbcglobal.net>
2017-11-03 21:00:22 -0400

    academic rubric

---
+++ app/app.js
@@ -0,0 +1,21 @@
+angular.module("lacc", ['ui.router'])
+
+.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider
+        .otherwise('/');
+
+    $stateProvider
+        .state('login', {
+            url: '/',
+            templateUrl: './templates/login.html',
+        });
+}])
+
+.run(['$rootScope', function($rootScope) {
+
+}])
+
+.controller('main', ['$scope', "$state", "$rootScope", function($scope, $state, $rootScope) {
+    $scope.foo = "hello";
+}])
+;
