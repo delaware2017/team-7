e465a5871149ad6f7e9ac0480ed806ac0596ddaa
Myriam Benkoussa <benkoussam@sbcglobal.net>
2017-11-04 08:30:34 -0400

    updated

--- app/controllers/academicRubric.js
+++ app/controllers/academicRubric.js
@@ -1,6 +1,17 @@
 var database = firebase.database();
 
-angular.module("lacc")
-.controller("graderController", ["$scope", "$rootScope", function($scope, $rootScope) {
+angular.module("lacc").controller("academicRubricController", ["$scope", "$rootScope", function($scope, $rootScope) {
 //	$scope.people = $rootScope.people;
+
+$scope.sumInputs = function() {
+    var result = document.getElementById('totalScore');
+    var sum = 0;
+    $('.form-control').each (function() {
+        var q = ($(this).val());
+        sum  = sum + parseInt(q);  
+      })
+    result.value = (sum);
+  }
+
+
 }]);
