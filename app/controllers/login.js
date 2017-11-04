0e6ddcb79fc19e4dd0833534e6ac26168f79036c
Asim Anis <asimness@gmail.com>
2017-11-04 00:48:48 -0500

    change login up

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -9,6 +9,7 @@ angular.module("lacc")
 		firebase.auth().signInWithEmailAndPassword(email, password)
 		.then(function(data) {
 			$rootScope.user = data;
+			console.log(data);
 			var ref = database.ref("users");
 			// Attach an asynchronous callback to read the data at our posts reference
 			ref.on("value", function(snapshot) {
