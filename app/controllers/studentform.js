d94233d74fdb41d515b2b179645d37423b71ca75
Mathew Varughese <mat.varughese3@gmail.com>
2017-11-04 06:29:36 -0400

    upload to fire

--- app/controllers/studentform.js
+++ app/controllers/studentform.js
@@ -11,7 +11,7 @@ angular.module("lacc")
 		$scope.save = function(){
 			console.log($scope.user);
 			var x = {"personalStatement":"gg","gpa":"4","classRank":"4","classesTaken":"g","honorsClassesTaken":"g","sat":"2200","academicAwards":"g","HonorSociety":"g","performingArtLink":"g","ScienceTechnologyAwards":"g","communityService":"g","atheletics":"g"};
-		}
+		};
 
 		$scope.addFile = function(fileName) {
 			var f = $('.filereader')[0].files[0],
@@ -29,13 +29,14 @@ angular.module("lacc")
 
 .directive('filechange', ["$parse", function($parse) {
 	return function(scope, element, attrs) {
+		var scopedata = $scope.user[attrs.ngModel];
 		element.bind('change', function() {
 			var files = element[0].files;
 			var fileName = attrs.ngModel;
 			Array.from(files).forEach(function(f) {
 				var ref = storageRef.child(fileName);
 				ref.put(f).then(function(snapshot) {
-				  console.log('Uploaded a blob or file!');
+					scopedata = snapshot.downloadURL;
 				});
 			});
 		});
