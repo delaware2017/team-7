244622b700906615cf8e00780f139c2ab4714af6
Mathew Varughese <mat.varughese3@gmail.com>
2017-11-04 05:57:16 -0400

    file upload

--- app/controllers/studentform.js
+++ app/controllers/studentform.js
@@ -1,14 +1,38 @@
 angular.module("lacc")
-.controller("studentFormController", ["$scope", "$rootScope", function($scope, $rootScope) {
-	$scope.add = function() {
-	    var f = $('.filereader')[0].files[0],
-	        r = new FileReader();
 
-	    r.onloadend = function(e) {
-	      var data = e.target.result;
-	      //send your binary data via $http or $resource or do anything else with it
-		  console.log(data);
-		    r.readAsBinaryString(f);
+.controller("studentFormController", [
+	"$scope",
+	"$rootScope",
+	function($scope, $rootScope) {
+		var files = {};
+
+		$scope.addFile = function(fileName) {
+			var f = $('.filereader')[0].files[0],
+				r = new FileReader();
+
+			r.onloadend = function(e) {
+				var data = e.target.result;
+				//send your binary data via $http or $resource or do anything else with it
+				console.log(data);
+				r.readAsBinaryString(f);
+			};
 		};
+	}
+])
+
+.directive('filechange', ["$parse", function($parse) {
+	return function(scope, element, attrs) {
+		element.bind('change', function() {
+			var f = element.files[0];
+			var r = new FileReader();
+			r.onloadend = function(e) {
+				var data = e.target.result;
+				//send your binary data via $http or $resource or do anything else with it
+				console.log(data);
+				r.readAsBinaryString(f);
+			};
+
+		});
 	};
-}]);
+}])
+;
