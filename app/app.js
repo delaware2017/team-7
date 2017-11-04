ce0587eb12dc89217d5427871a0bab14e997584e
Asim Anis <asimness@gmail.com>
2017-11-03 22:35:36 -0500

    homepage

--- app/app.js
+++ app/app.js
@@ -14,7 +14,14 @@ angular.module("lacc", ['ui.router'])
             url: '/register/:type',
             templateUrl: './templates/register.html',
 			controller: "loginController"
-        });
+        })
+        .state('homepage',{
+        	url: '/homepage',
+        	templateUrl: './templates/homepage.html',
+        	controller: "homepageController"
+        })
+
+        ;
 }])
 
 .run(['$rootScope', function($rootScope) {
