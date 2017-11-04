b1f0df7c432533e545bd9703ca2d4d21085ffd42
Mathew Varughese <mat.varughese3@gmail.com>
2017-11-04 09:08:49 -0400

    judges dont show up anymore

--- app/controllers/grader.js
+++ app/controllers/grader.js
@@ -3,6 +3,6 @@ var database = firebase.database();
 angular.module("lacc")
 .controller("graderController", ["$scope", "$rootScope", "$stateParams", function($scope, $rootScope, $stateParams) {
 	var userid = $stateParams.id;
-	var data = $rootScope.listofusers[userid];
+	var data = $rootScope.people[userid];
 	$scope.student = data;
 }]);
