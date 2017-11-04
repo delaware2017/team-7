96441b86e876871ccbbe01bcc41a1274211bd0de
Mathew Varughese <mat.varughese3@gmail.com>
2017-11-04 05:30:23 -0400

    wrong file

--- app/controllers/studentform.js
+++ app/controllers/studentform.js
@@ -1,14 +1,18 @@
 angular.module("lacc")
 .controller("studentFormController", ["$scope", "$rootScope", function($scope, $rootScope) {
 	$scope.add = function() {
-    var f = $('.filereader')[0].files[0],
-        r = new FileReader();
+	    var f = $('.filereader')[0].files[0],
+	        r = new FileReader();
 
-    r.onloadend = function(e) {
-      var data = e.target.result;
-      //send your binary data via $http or $resource or do anything else with it
-    }
+	    r.onloadend = function(e) {
+	      var data = e.target.result;
+	      //send your binary data via $http or $resource or do anything else with it
 
-    r.readAsBinaryString(f);
-}
+	    r.readAsBinaryString(f);
+		};
+	};
+
+	$scope.save = function() {
+		console.log($scope.user);
+	};
 }]);
