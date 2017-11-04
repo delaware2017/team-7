af0b59d100b64a4c424dc74d1fe52310c291a17c
Aadit Vyas <aadit.vyas@yale.edu>
2017-11-04 06:53:01 -0400

    Updated Syntax

--- app/templates/script.js
+++ app/templates/script.js
@@ -1,23 +1,22 @@
 
 function checkCredentials() {
+	status = 1;
+	x = document.getElementById("text");
 
-status = 1;
-x = document.getElementById("text");
+	if(status==1) {
+	    x.style.color = 'blue';
+	    status = 2;
+	}
 
-if(status==1) {
-    x.style.color = 'blue';
-    status = 2;
-}
+	if(status==2) {
+	    x.style.color = 'red';
+	    status = 3;
+	}
 
-if(status==2) {
-    x.style.color = 'red';
-    status = 3;
-}
-
-if(status==3) {
-    x.style.color = 'yellow';
-    status = 1;
-}
+	if(status==3) {
+	    x.style.color = 'yellow';
+	    status = 1;
+	}
 
 }
 
