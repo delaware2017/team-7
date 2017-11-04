c6a3a90b2a1fc513a9d0735b5d60b55011836da0
Asim Anis <asimness@gmail.com>
2017-11-04 02:08:47 -0500

    made read file work

--- app/controllers/studentform.js
+++ app/controllers/studentform.js
@@ -1,4 +1,14 @@
 angular.module("lacc")
 .controller("studentFormController", ["$scope", "$rootScope", function($scope, $rootScope) {
-	
+	$scope.add = function() {
+    var f = $('.filereader')[0].files[0],
+        r = new FileReader();
+
+    r.onloadend = function(e) {
+      var data = e.target.result;
+      //send your binary data via $http or $resource or do anything else with it
+    }
+
+    r.readAsBinaryString(f);
+}
 }]);
